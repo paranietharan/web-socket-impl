561fe953d2a890fdfff32a8d871bb701c4241ac9
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-08-03 19:35:12 +0530

    add and remove client logic

--- manager.go
+++ manager.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -15,11 +16,14 @@ var (
 )
 
 type Manager struct {
-	//
+	clients ClientList
+	sync.RWMutex
 }
 
 func NewManager() *Manager {
-	return &Manager{}
+	return &Manager{
+		clients: make(ClientList),
+	}
 }
 
 func (m *Manager) serverWS(w http.ResponseWriter, r *http.Request) {
@@ -31,5 +35,23 @@ func (m *Manager) serverWS(w http.ResponseWriter, r *http.Request) {
 		log.Println(err)
 	}
 
-	conn.Close()
+	client := NewClient(conn, m)
+	m.addClient(client)
+}
+
+func (m *Manager) addClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	m.clients[client] = true
+}
+
+func (m *Manager) removeClient(client *Client) {
+	m.Lock()
+	defer m.Unlock()
+
+	if _, ok := m.clients[client]; ok {
+		client.connection.Close()
+		delete(m.clients, client)
+	}
 }
