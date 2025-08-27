cc99d0d29242e6be6af23e1f619dfe24427b9f93
Paranietharan <142080100+paranietharan@users.noreply.github.com>
2025-08-27 22:48:00 +0530

    TLS Impl & otp_unit test

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 	setupAPI(ctx)
 	log.Println("Server started......")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServeTLS(":8080", "server.crt", "server.key", nil))
 }
 
 func setupAPI(ctx context.Context) {
